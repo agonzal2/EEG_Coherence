c4aa78821528cf4cde5936af17c729918e17da8d
jmaicas <jorgemaicas@gmail.com>
2020-10-11 21:05:12 +0100

    Batch permutation analysis added
    Goes through all xlsx files in a folder and calculates the permutation analysis of the coherences in those files. 
    
    Export png, pdf and a single excel file with one sheet per data file in the folder

--- stats_analysis.py
+++ stats_analysis.py
@@ -1,10 +1,14 @@
 import sys
+import os
+import glob
+import re as re
 import numpy as np
 import random
 import pandas as pd
 from scipy.stats import ttest_ind
 from statistics import mean
 import matplotlib.pyplot as plt
+import xlsxwriter
 
 from w_perm_analysis import *
 
@@ -15,6 +19,8 @@ from PyQt5.QtCore import pyqtSlot
 
 import matplotlib.pyplot as plt
 import matplotlib.backends.backend_pdf
+
+from datetime import datetime
 
 def calc_perm_test(perm_averages):
     half_len = int(len(perm_averages)/2)
@@ -29,13 +35,39 @@ def calc_perm_test(perm_averages):
     diff_perm = mean(avg_sec) - mean(avg_first)
     return diff_perm
 
+def get_metadata(filename):
+  '''
+    obtains metadata from the name of a file
+    input : filename
+    returns three strings: brain state, long_distance and kind of coherence, absolute or imaginary
+
+  '''
+  if "_REM" in filename:
+        brain_state = "REM"
+  elif "NonREM" in filename:
+        brain_state = "NonREM"
+
+  for distance in np.arange(1, 6, 0.5):
+    if str(distance)[:3] in filename:
+      long_distance = str(distance)[:3]
+      break
+
+  if "abs" in filename:
+    coh_type = "abs"
+  else:
+    coh_type = "imag"
+
+  return brain_state, long_distance, coh_type
+
+
+
 class MyForm(QMainWindow):
   def __init__(self):
     super().__init__()
     self.ui = Ui_w_perm_Analysis()
     self.ui.setupUi(self)
 
-    self.ui.ButSelectFile.clicked.connect(self.selectFile)
+    self.ui.ButtonSelectFolder.clicked.connect(self.selectFolder)
     self.ui.ButDelFile.clicked.connect(self.delFile)
     self.ui.ButtonRunAll.clicked.connect(self.runAll)
     self.ui.ButtonClearFigures.clicked.connect(self.closeFigures)
@@ -50,40 +82,61 @@ class MyForm(QMainWindow):
     self.show()
 
 
-  def selectFile(self):
-    file_name, _ = QFileDialog.getOpenFileName(self,
-                                     'Select file',
-                                     './',
-                                     'Excel Files (*.xls *.xlsx)')
-    if file_name:
-      self.ui.labelFileSelected.setText(file_name)
+  def selectFolder(self):
+    DataFolder = str(QFileDialog.getExistingDirectory(self, "Select Directory"))
+    if DataFolder:
+      self.ui.labelFileSelected.setText(DataFolder)
     else:
-      self.error_msg.showMessage("It is necessary to select a file")
+      self.error_msg.showMessage("It is necessary to select a folder")
 
   def delFile(self):
     self.ui.labelFileSelected.clear()
 
   def runAll(self):
-    self.dfs = pd.read_excel(self.ui.labelFileSelected.text(), sheet_name=None, index_col=None, header=None)
+    os.chdir(str(self.ui.labelFileSelected.text()))
+    d = os.getcwd() + "\\"
+    matching_files = glob.glob(r'*xlsx')
+    l_xlsx_files = []
+    self.workbook = xlsxwriter.Workbook('permutation_stats.xlsx')
+    for matching_file in matching_files:
+      file_n = re.sub('.xlsx', '', matching_file)
+      file_name = 'perm_analysis_' + file_n
+      self.brain_state, self.longD, self.coh_type = get_metadata(file_n)
+      sheet_n = self.brain_state + self.longD + self.coh_type
+      file_route = d + matching_file
+      l_xlsx_files.append(file_route)
 
-    self.compareGroups('ShortKO', 'ShortWT')
-    self.write_table_results(3)
-    self.compareGroups('LongKO', 'LongWT')
-    self.write_table_results(4)
+      self.dfs = pd.read_excel(file_route, sheet_name=None)
+      self.sheet_name = sheet_n + "Short"
+      self.compareGroups('ShortKO', 'ShortWT')
+      self.write_table_results(3)
+      self.sheet_name = sheet_n + "Long"
+      self.compareGroups('LongKO', 'LongWT')
+      self.write_table_results(4)
+      self.print2pdf(file_name)
+      self.closeFigures()
+
+    self.workbook.close()
+    print ("Data and figures correctly exported to excel")
 
   def compareGroups(self, group1, group2):
 
     df_g1 = self.dfs[group1] # KO
-    df_g1.drop(df_g1.columns[0], axis=1, inplace=True)
     df_g2 = self.dfs[group2] # WT
-    freq_l = df_g2[0].tolist()
+
+    df_g1.reset_index(inplace=True) # the pandas has a multiindex, including the Freqs column.
+    df_g2.reset_index(inplace=True)
+    # Deleting all that information that we do not need
+    df_g1.drop(['index', 'Unnamed: 0', 'Freqs'], axis=1, inplace=True)
+    df_g2.drop(['index', 'Unnamed: 0', 'Freqs'], axis=1, inplace=True)
+
+    freq_l =  df_g1.index.values # df_g2[0].tolist()
     df_means = pd.DataFrame(freq_l)
     df_means.columns = ['Frequency (Hz)']
-    df_g2.drop(df_g2.columns[0], axis=1, inplace=True)
-    orig_cols = list(df_g1.columns)
+    #df_g2.drop(df_g2.columns[0], axis=1, inplace=True)
     new_cols = []
-    for i in orig_cols:
-      new_cols.append(i+ len(df_g2.columns))
+    for i in np.arange(len(df_g1.columns)):
+      new_cols.append("n"+str(i + 1 + len(df_g2.columns)))
 
     df_g1.columns = new_cols
 
@@ -95,7 +148,10 @@ class MyForm(QMainWindow):
     df_means[column1] = df_g1.mean(axis=1)
     df_means[column2] = df_g2.mean(axis=1)
     colors = ['black', '#0836a9']
-    dx = df_means.plot(x='Frequency (Hz)', y =[column2, column1], ylim = (0,1), color = colors)
+    if 'abs' in self.coh_type:
+      dx = df_means.plot(x='Frequency (Hz)', y =[column2, column1], ylim = (0,1), color = colors)
+    else:
+      dx = df_means.plot(x='Frequency (Hz)', y =[column2, column1], color = colors)
     dx.set_ylabel('Mean $z^{-1}$ Coherence')
     plt.show()
 
@@ -142,11 +198,6 @@ class MyForm(QMainWindow):
 
       max_distribution.append(max(max_candidates))
 
-    for n_band, freq_band in enumerate(self.freq_list):
-      p_val = pvalues[n_band] / self.ui.BoxNumberShuffles.value()
-      self.freq_list_results.append(p_val)
-      #print('The p-value for freqs between ', freq_1, ' and ', freq_2, 'Hz is: ', p_val)
-
     # plotting max distribution
     fig1 = plt.figure()
     bx = fig1.add_subplot(111)
@@ -160,19 +211,34 @@ class MyForm(QMainWindow):
     bx.plot([perc95, perc95], [0,max_height], color = '#888888', linestyle='dashed', lw=1)
     plt.show
 
-    print('if the difference is bigger than the threshold (', perc95, ') then there is significance')
+    # Calculating pvalues, without correction and exporting to excel
+    worksheet_ti= self.workbook.add_worksheet(self.sheet_name)
+    header = 'Correction for multiple comparisons: if the difference is bigger than the 95 percentile threshold ('+ str(perc95) +') then there is significance'
+    worksheet_ti.write(0,0,header)
+    worksheet_ti.write(1, 0, "freq band")
+    worksheet_ti.write(1, 1, "Freq_1")
+    worksheet_ti.write(1, 2, "Freq_2")
+    worksheet_ti.write(1, 3, "p_value")
+    worksheet_ti.write(1, 4, "difference")
     for n_band, freq_band in enumerate(self.freq_list):
-      print(freq_band[0], 'diff= ', real_diff[n_band])
+      p_val = pvalues[n_band] / self.ui.BoxNumberShuffles.value()
+      self.freq_list_results.append(p_val)
+      #print('The p-value for freqs between ', freq_1, ' and ', freq_2, 'Hz is: ', p_val)
+      worksheet_ti.write(n_band + 2, 0, freq_band[0])
+      worksheet_ti.write(n_band + 2, 1, freq_band[1])
+      worksheet_ti.write(n_band + 2, 2, freq_band[2])
+      worksheet_ti.write(n_band + 2, 3, p_val)
+      worksheet_ti.write(n_band + 2, 4, real_diff[n_band])
 
     # plotting bars graph
     for freq_band in self.freq_list:
-        f_1, f_2 = self.obtainfreqs(freq_band)
+      f_1, f_2 = self.obtainfreqs(freq_band)
 
-        labels.append(freq_band[0])
-        mean_KO_l.append(float(df_means[[column1]].iloc[range(f_1,f_2,1)].mean(axis=0)))
-        mean_WT_l.append(float(df_means[[column2]].iloc[range(f_1,f_2,1)].mean(axis=0)))
-        sem_KO_l.append(float(df_means[[column1]].iloc[range(f_1,f_2,1)].sem(axis=0)))
-        sem_WT_l.append(float(df_means[[column2]].iloc[range(f_1,f_2,1)].sem(axis=0)))
+      labels.append(freq_band[0])
+      mean_KO_l.append(float(df_means[[column1]].iloc[range(f_1,f_2,1)].mean(axis=0)))
+      mean_WT_l.append(float(df_means[[column2]].iloc[range(f_1,f_2,1)].mean(axis=0)))
+      sem_KO_l.append(float(df_means[[column1]].iloc[range(f_1,f_2,1)].sem(axis=0)))
+      sem_WT_l.append(float(df_means[[column2]].iloc[range(f_1,f_2,1)].sem(axis=0)))
     width = 0.35
     x = np.arange(len(labels))
 
@@ -180,13 +246,29 @@ class MyForm(QMainWindow):
     rects1 = ax.bar(x - width/2, mean_WT_l, width, yerr=sem_WT_l, capsize = 3, label='$Syngap^{+/+}$', color='black')
     rects2 = ax.bar(x + width/2, mean_KO_l, width, yerr=sem_KO_l, capsize = 3, label='$Syngap^{+/-\u0394 GAP}$', color= '#0836a9')
     ax.set_ylabel('Mean $z^{-1}$ Coherence')
-    ax.set_ylim([0,1])
+    if 'abs' in self.coh_type:
+      ax.set_ylim([0,1])
     ax.set_xticks(x)
     ax.set_xticklabels(labels)
 
     ax.legend()
     fig.tight_layout()
     plt.show()
+
+    # Once the figures are plotted, include them in the excel sheet
+    cell_positions = ('G3', 'O3', 'W3','G30', 'O30', 'W30')
+    figFolder = os.getcwd()
+    if figFolder:
+        prefix = '/' + str(datetime.now().strftime('%Y_%m_%d_%H_%M_%S'))
+        j = 0
+        for i in plt.get_fignums():
+            plt.figure(i)
+            file_name = figFolder + prefix +'figure%d.png' % i
+            plt.savefig(file_name)
+            worksheet_ti.insert_image(cell_positions[j], file_name)
+            j = j+1
+    else:
+        self.error_msg.showMessage("Something went wrong saving the figure")
 
   def obtainfreqs(self, freq_band):
     freq_1 = freq_band[1]
@@ -218,17 +300,16 @@ class MyForm(QMainWindow):
   def closeFigures(self):
        plt.close('all')
 
-  def print2pdf(self):
-       figFolder = str(QFileDialog.getExistingDirectory(self, "Select Directory"))
-       if figFolder:
-            filename = '/' + str(datetime.now().strftime('%Y_%m_%d_%H_%M_%S')) + '.pdf'
-            pdf = matplotlib.backends.backend_pdf.PdfPages(figFolder + filename)
-            figs = [plt.figure(n) for n in plt.get_fignums()]
-            for fig in figs:
-                 fig.savefig(pdf, format='pdf')
-            pdf.close()
-       else:
-            self.error_msg.showMessage("It is necessary to select a folder")
+  def print2pdf(self, filename = ''):
+    if filename:
+      filename2 = '/' + filename + '.pdf'
+      pdf = matplotlib.backends.backend_pdf.PdfPages(str(self.ui.labelFileSelected.text()) + filename2)
+      figs = [plt.figure(n) for n in plt.get_fignums()]
+      for fig in figs:
+        fig.savefig(pdf, format='pdf')
+      pdf.close()
+    else:
+      self.error_msg.showMessage("It is necessary to select a folder")
 
   def print2png(self):
        figFolder = str(QFileDialog.getExistingDirectory(self, "Select Directory"))
