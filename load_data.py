94302e9b63da7defffb0faab7542625bb78dd5a3
jmaicas <jorgemaicas@gmail.com>
2021-06-28 09:39:18 +0100

    Add files via upload

--- load_data.py
+++ load_data.py
@@ -8,7 +8,7 @@ import glob
 from initial_processes import *
 
 folder = '/media/jorge/DATADRIVE0/Data/Alfredo/Syngap/KOTest/'
-
+file_types = 'wakeconv' # all, wakeconv, sleep, everyone
 
 montage_name = '/media/jorge/DATADRIVE0/Code/MNE_Alfredo/standard_32grid_Alfredo.elc'
 n_electrodes = 32
@@ -34,17 +34,28 @@ for matching_file in matching_files:
 for i, xls_file in enumerate(l_xls_files):
     # First extract data for the whole recording but just for the NonREM state
     brain_states = import_brain_states(xls_file)
-    v_all, v_wake, v_nrem, v_rem, v_conv = load_32_EEG_downsampled_bystate(l_folders[i], montage_name, '100', brain_states, downsampling, amp_filter)
-    f_all = l_numpy_files[i] + '.npy'
-    f_wake = l_numpy_files[i] + '_wake' + '.npy'
-    f_nrem = l_numpy_files[i] + '_nrem' + '.npy'
-    f_rem = l_numpy_files[i] + '_rem' + '.npy'
-    f_conv = l_numpy_files[i] + '_conv' + '.npy'
-    np.save(f_all, v_all)
-    np.save(f_wake, v_wake)
-    np.save(f_nrem, v_nrem)
-    np.save(f_rem, v_rem)
-    np.save(f_conv, v_conv)
-
+    if file_types == 'wakeconv':
+        v_wakeconv = load_32_EEG_downsampled_wake_conv(l_folders[i], montage_name, '100', brain_states, downsampling, amp_filter)    
+        f_wakeconv = l_numpy_files[i] + '_wake_conv' + '.npy'
+        np.save(f_wakeconv, v_wakeconv)
+    elif file_types == 'everyone':
+        v_all, v_wake, v_nrem, v_rem, v_conv = load_32_EEG_downsampled_bystate(l_folders[i], montage_name, '100', brain_states, downsampling, amp_filter)
+        f_all = l_numpy_files[i] + '.npy'
+        f_wake = l_numpy_files[i] + '_wake' + '.npy'
+        f_nrem = l_numpy_files[i] + '_nrem' + '.npy'
+        f_rem = l_numpy_files[i] + '_rem' + '.npy'
+        f_conv = l_numpy_files[i] + '_conv' + '.npy'
+        np.save(f_all, v_all)
+        np.save(f_wake, v_wake)
+        np.save(f_nrem, v_nrem)
+        np.save(f_rem, v_rem)
+        np.save(f_conv, v_conv)
+    elif file_types == 'sleep':
+        v_nrem, v_rem = load_32_EEG_downsampled_sleep(l_folders[i], montage_name, '100', brain_states, downsampling, amp_filter)
+        f_nrem = l_numpy_files[i] + '_nrem' + '.npy'
+        f_rem = l_numpy_files[i] + '_rem' + '.npy'
+        np.save(f_nrem, v_nrem)
+        np.save(f_rem, v_rem)
+        
 
 
